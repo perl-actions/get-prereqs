49626db561f928e66c949912c2705526bea4056d
Graham Knop <haarg@haarg.org>
2024-09-13 11:07:05 +0200

    testing version routine

---
+++ test/cpan-versions.test.mjs
@@ -0,0 +1,9 @@
+import assert from 'node:assert';
+import { dottedVersion } from '../src/cpan-versions.mjs';
+
+describe('perl version', function () {
+  it('converted correctly', async function() {
+    assert.strictEqual(dottedVersion('>=5.006'), 'v5.6');
+  });
+});
+
