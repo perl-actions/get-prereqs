e874e9ce3fa68e67b3591a72d608d614462e5061
Graham Knop <haarg@haarg.org>
2024-10-07 20:06:46 +0200

    improve action mocking
    add getBooleanInput function and default handling

--- test/action.test.mjs
+++ test/action.test.mjs
@@ -2,7 +2,9 @@ import { describe, it } from 'mocha';
 import { expect } from 'chai';
 
 import { dirname, join as joinPath } from 'node:path';
+import * as fs from 'node:fs/promises';
 import { fileURLToPath } from 'url';
+import yaml from 'js-yaml';
 
 import esmock from 'esmock';
 
@@ -12,10 +14,46 @@ const mockAction = async (module) => {
   let outputs;
   let inputs;
   let failed;
+  const actionConfig = yaml.load(await fs.readFile(joinPath(__dirname, '../action.yml'), 'utf8'));
+
+  const getInput = (key, { required } = {}) => {
+    const inputConfig = actionConfig.inputs[key];
+    if (!inputConfig) {
+      throw new Error(`invalid input ${key}`);
+    }
+    if (Object.hasOwn(inputs, key)) {
+      return inputs[key];
+    }
+    else if (required) {
+      throw new Error(`input ${key} is required but was not provided!`);
+    }
+    else if (Object.hasOwn(inputConfig, 'default')) {
+      return inputConfig['default'];
+    }
+    else {
+      return '';
+    }
+  };
+
   const main = await esmock(module, {
     '@actions/core': {
-      getInput:          key => (inputs[key] || '').trim(),
-      getMultilineInput: key => (inputs[key] || '').split(/\n/).map(t => t.trim()),
+      getInput:        (key, ...rest) => (getInput(key, ...rest) || '').trim(),
+      getBooleanInput: (key, ...rest) => {
+        const value = getInput(key, ...rest);
+        if (value === true || value === false) {
+          return value;
+        }
+        else if (value === 'true' || value === 'True' || value === 'TRUE') {
+          return true;
+        }
+        else if (value === 'false' || value === 'False' || value === 'FALSE') {
+          return false;
+        }
+        else {
+          throw new Error(`invalid value for ${key}`);
+        }
+      },
+      getMultilineInput: (key, ...rest) => (getInput(key, ...rest) || '').split(/\n/).map(t => t.trim()),
       setOutput:         (key, value) => { outputs[key] = value; },
       setFailed:         (message) => { failed = message; },
     },
