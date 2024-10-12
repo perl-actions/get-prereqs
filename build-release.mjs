85ba00e04bd6a85ae62efbb929024234d0bdb73e
Graham Knop <haarg@haarg.org>
2024-10-12 12:05:33 +0200

    output is a module

--- build-release.mjs
+++ build-release.mjs
@@ -4,15 +4,16 @@ import esbuild from 'esbuild';
 import { esbuildPeggyPlugin } from './src/peggy-compile.mjs';
 
 const config = {
-  entryPoints: ['src/action.mjs'],
-  format:      'esm',
-  outdir:      'dist',
-  bundle:      true,
-  platform:    'node',
-  target:      ['node20'],
-  dropLabels:  ['NOCOMPILE'],
-  minify:      true,
-  plugins:     [
+  entryPoints:  ['src/action.mjs'],
+  format:       'esm',
+  outdir:       'dist',
+  outExtension: { '.js': '.mjs' },
+  bundle:       true,
+  platform:     'node',
+  target:       ['node20'],
+  dropLabels:   ['NOCOMPILE'],
+  minify:       true,
+  plugins:      [
     esbuildPeggyPlugin(),
   ],
 };
